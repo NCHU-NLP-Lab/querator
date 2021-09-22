3a775fd6b9f63d86b421e7bbe911d2cbe1050b28
Tomy Hsieh <tomy0000000@gmail.com>
2021-09-22 21:59:59 +0800

    🧹 chore: Update Footer

--- react/src/module/FooterModule/index.jsx
+++ react/src/module/FooterModule/index.jsx
@@ -19,21 +19,15 @@ class Index extends Component {
                         rel="noopener noreferrer"
                         target="_blank"
                         href="http://udiclab.cs.nchu.edu.tw/querator.html"
-                        className="footer-text">
-                        <span className="giticon">Querator AI</span>
+                        className="footer-brand">
+                        Querator AI
                     </a>
                     <hr />
-                    <div className="f-small">
-                        <a
-                            rel="noopener noreferrer"
-                            target="_blank"
-                            href="http://udiclab.cs.nchu.edu.tw"
-                            className="footer-text">
-                        <small><IoIosPin /> 中興大學，理學大樓720室 UDIC LAB</small>
-                        <br />
-                        <small><MdCall /> 04-22840497轉720</small>
-                        </a>
-                    </div>
+                    <p className="footer-text">
+                        Copyright © 2021 
+                        <a href="https://nlpnchu.org/">NCHU NLP Lab</a>.
+                        All rights reserved.
+                    </p>
                 </div>
             </div>
         );
