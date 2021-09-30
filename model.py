7ddb08a056c9a621a881c11396879327f6678530
Tomy Hsieh <tomy0000000@gmail.com>
2021-10-01 01:38:01 +0800

    💅🏻 style: Black format

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
+from typing import List, Optional
+
 from pydantic import BaseModel
-from typing import Optional,List
+
 
 class QuestionAndAnswer(BaseModel):
     tag: str
@@ -7,76 +9,68 @@ class QuestionAndAnswer(BaseModel):
     end_at: int
     questions: List[str] = []
 
+
 class Answer(BaseModel):
     tag: str
     start_at: int
     end_at: int
 
+
 class QGItem(BaseModel):
-    article:str
-    answer:Answer
+    article: str
+    answer: Answer
+
 
 class EnQGItem(QGItem):
-     class Config:
+    class Config:
         schema_extra = {
             "example": {
                 "article": "Harry Potter is a series of seven fantasy novels written by British author, J. K. Rowling.",
-                "answer":  {
-                    "tag": "J. K. Rowling",
-                    "start_at": 76,
-                    "end_at": 88
-                }
+                "answer": {"tag": "J. K. Rowling", "start_at": 76, "end_at": 88},
             }
         }
+
 
 class ZhQGItem(QGItem):
-     class Config:
+    class Config:
         schema_extra = {
-            "example":{
-                "article":"英國作家J·K·羅琳的兒童奇幻文學系列小說，描寫主角哈利波特在霍格華茲魔法學校7年學習生活中的冒險故事；該系列被翻譯成75種語言",
-                "answer":{
-                    "tag":"冒險故事",
-                    "start_at":47,
-                    "end_at":50
-                }
+            "example": {
+                "article": "英國作家J·K·羅琳的兒童奇幻文學系列小說，描寫主角哈利波特在霍格華茲魔法學校7年學習生活中的冒險故事；該系列被翻譯成75種語言",
+                "answer": {"tag": "冒險故事", "start_at": 47, "end_at": 50},
             }
         }
 
+
 class DisItem(BaseModel):
-    article:str
-    answer:Answer
-    question:str
-    gen_quantity:int
+    article: str
+    answer: Answer
+    question: str
+    gen_quantity: int
+
 
 class EnDisItem(DisItem):
     class Config:
         schema_extra = {
-            "example":{
+            "example": {
                 "article": "Harry Potter is a series of seven fantasy novels written by British author, J. K. Rowling.",
-                "answer":  {
-                    "tag": "J. K. Rowling",
-                    "start_at": 76,
-                    "end_at": 88
-                },
+                "answer": {"tag": "J. K. Rowling", "start_at": 76, "end_at": 88},
                 "question": "Who wrote Harry Potter?",
-                "gen_quantity":3
+                "gen_quantity": 3,
             }
         }
+
 
 class ZhDisItem(DisItem):
     class Config:
         schema_extra = {
-            "example":{
+            "example": {
                 "article": "英國作家J·K·羅琳的兒童奇幻文學系列小說，描寫主角哈利波特在霍格華茲魔法學校7年學習生活中的冒險故事；該系列被翻譯成75種語言",
-                "answer":{
-                    "tag":"冒險故事",
-                    "start_at":47,
-                    "end_at":50
-                },
+                "answer": {"tag": "冒險故事", "start_at": 47, "end_at": 50},
                 "question": "哈利波特是一本怎麼樣的小說?",
-                "gen_quantity":3
+                "gen_quantity": 3,
             }
-        }        
+        }
+
 
 class Distractors(BaseModel):
     distractors: List[str]
--- utils.py
+++ utils.py
@@ -1,88 +1,115 @@
-from loguru import logger
-import torch
-from config import max_length,hl_token
-import os
-import wget
-from nlgeval import NLGEval
-from transformers import RobertaTokenizer, AutoTokenizer
-from transformers import RobertaForMultipleChoice,AutoModelForSeq2SeqLM
-from torch.distributions import Categorical
 import itertools as it
-import nlp2go
-from functools import lru_cache
 import json
+import os
+from functools import lru_cache
+
+import torch
+from loguru import logger
+from nlgeval import NLGEval
+from torch.distributions import Categorical
+from transformers import (
+    AutoModelForSeq2SeqLM,
+    AutoTokenizer,
+    RobertaForMultipleChoice,
+    RobertaTokenizer,
+)
+
+from config import hl_token, max_length
 from model import Answer
 
-def prepare_qg_model_input_ids(article,start_at,end_at,tokenizer):
+
+def prepare_qg_model_input_ids(article, start_at, end_at, tokenizer):
     hl_context = f"{article[:start_at]}{hl_token}{article[start_at:end_at]}{hl_token}{article[end_at:]}"
     logger.info(hl_context)
-    model_input = tokenizer(
-        hl_context,
-        return_length=True
-    )
+    model_input = tokenizer(hl_context, return_length=True)
 
-    input_length = model_input['length'][0]
+    input_length = model_input["length"][0]
     if input_length > max_length:
         hl_token_id = tokenizer.convert_tokens_to_ids([hl_token])[0]
-        slice_length = int(max_length/2)
-        mid_index = model_input['input_ids'].index(hl_token_id)
-        new_input_ids = model_input['input_ids'][mid_index-slice_length:mid_index+slice_length]
-        model_input['input_ids'] = new_input_ids
-    return torch.LongTensor([model_input['input_ids']]), input_length
+        slice_length = int(max_length / 2)
+        mid_index = model_input["input_ids"].index(hl_token_id)
+        new_input_ids = model_input["input_ids"][
+            mid_index - slice_length : mid_index + slice_length
+        ]
+        model_input["input_ids"] = new_input_ids
+    return torch.LongTensor([model_input["input_ids"]]), input_length
 
-def prepare_dis_model_input_ids(article,question,answer,ans_start,ans_end,tokenizer):
+
+def prepare_dis_model_input_ids(
+    article, question, answer, ans_start, ans_end, tokenizer
+):
     sep_token_id = tokenizer.sep_token_id
-    article_max_length = max_length - 52 # 後面會手動插入2個sep_token
-    article_max_length -= 20 # 預留20個token空間
+    article_max_length = max_length - 52  # 後面會手動插入2個sep_token
+    article_max_length -= 20  # 預留20個token空間
     article_input = tokenizer(
-        tokenizer.cls_token+article,
-        add_special_tokens=False,
-        return_length=True
+        tokenizer.cls_token + article, add_special_tokens=False, return_length=True
     )
     # logger.debug(article_input)
-    article_length = article_input['length']
+    article_length = article_input["length"]
     if type(article_length) is list:
         article_length = article_length[0]
-    
+
     # 當文章過長，依據答案位置重新裁切文章
     if article_length > article_max_length:
-        slice_length = int(article_max_length/2)
-        mid_index = int((ans_start + ans_end)/2)
-        new_input_ids = article_input['input_ids'][mid_index-slice_length:mid_index+slice_length]
-        article_input['input_ids'] = new_input_ids
+        slice_length = int(article_max_length / 2)
+        mid_index = int((ans_start + ans_end) / 2)
+        new_input_ids = article_input["input_ids"][
+            mid_index - slice_length : mid_index + slice_length
+        ]
+        article_input["input_ids"] = new_input_ids
 
     question_input = tokenizer(
         question,
         max_length=30,
         return_length=True,
         add_special_tokens=False,
-        truncation=True
+        truncation=True,
     )
-    question_length = question_input['length']
+    question_length = question_input["length"]
 
     answer_input = tokenizer(
         answer,
         max_length=20,
         return_length=True,
         add_special_tokens=False,
-        truncation=True
+        truncation=True,
     )
-    answer_length = answer_input['length']
-    
-    final_input_ids = article_input['input_ids'] + [sep_token_id] + question_input['input_ids'] + [sep_token_id] + answer_input['input_ids']
+    answer_length = answer_input["length"]
+
+    final_input_ids = (
+        article_input["input_ids"]
+        + [sep_token_id]
+        + question_input["input_ids"]
+        + [sep_token_id]
+        + answer_input["input_ids"]
+    )
     total_legnth = len(final_input_ids)
     assert total_legnth <= max_length
     return torch.LongTensor([final_input_ids]), total_legnth
 
-class BartDistractorGeneration():
+
+class BartDistractorGeneration:
     def __init__(self):
-        self.nlgeval = NLGEval(metrics_to_omit=['METEOR', 'EmbeddingAverageCosineSimilairty', 'SkipThoughtCS', 'VectorExtremaCosineSimilarity','GreedyMatchingScore', 'CIDEr'])
-    
+        self.nlgeval = NLGEval(
+            metrics_to_omit=[
+                "METEOR",
+                "EmbeddingAverageCosineSimilairty",
+                "SkipThoughtCS",
+                "VectorExtremaCosineSimilarity",
+                "GreedyMatchingScore",
+                "CIDEr",
+            ]
+        )
+
         #
         self.dg_models = [
             AutoModelForSeq2SeqLM.from_pretrained("voidful/bart-distractor-generation"),
-            AutoModelForSeq2SeqLM.from_pretrained("voidful/bart-distractor-generation-pm"),
-            AutoModelForSeq2SeqLM.from_pretrained("voidful/bart-distractor-generation-both"),
+            AutoModelForSeq2SeqLM.from_pretrained(
+                "voidful/bart-distractor-generation-pm"
+            ),
+            AutoModelForSeq2SeqLM.from_pretrained(
+                "voidful/bart-distractor-generation-both"
+            ),
         ]
 
         self.dg_tokenizers = [
@@ -92,47 +119,70 @@ class BartDistractorGeneration():
         ]
 
         for dg_model in self.dg_models:
-            dg_model.to(os.environ['BDG_DEVICE'])
+            dg_model.to(os.environ["BDG_DEVICE"])
 
         #
-        self.tokenizer = RobertaTokenizer.from_pretrained("LIAMF-USP/roberta-large-finetuned-race")
-        self.model = RobertaForMultipleChoice.from_pretrained("LIAMF-USP/roberta-large-finetuned-race")
+        self.tokenizer = RobertaTokenizer.from_pretrained(
+            "LIAMF-USP/roberta-large-finetuned-race"
+        )
+        self.model = RobertaForMultipleChoice.from_pretrained(
+            "LIAMF-USP/roberta-large-finetuned-race"
+        )
         self.model.eval()
-        self.model.to(os.environ['BDG_CLF_DEVICE'])
-    
+        self.model.to(os.environ["BDG_CLF_DEVICE"])
+
     @lru_cache(maxsize=1000)
-    def generate_distractor(self,context, question, answer, gen_quantity):
+    def generate_distractor(self, context, question, answer, gen_quantity):
         if type(answer) is str:
             answer = Answer.parse_obj(json.loads(answer))
 
         all_options = []
-        for i,(dg_tokenizer,dg_model) in enumerate(zip(self.dg_tokenizers,self.dg_models)):
-            d_input_ids,_ = prepare_dis_model_input_ids(context,question,answer.tag,answer.start_at,answer.end_at,dg_tokenizer)  # 如果文章過長進行重新裁切與處理
+        for i, (dg_tokenizer, dg_model) in enumerate(
+            zip(self.dg_tokenizers, self.dg_models)
+        ):
+            d_input_ids, _ = prepare_dis_model_input_ids(
+                context,
+                question,
+                answer.tag,
+                answer.start_at,
+                answer.end_at,
+                dg_tokenizer,
+            )  # 如果文章過長進行重新裁切與處理
             out_ids = dg_model.generate(
-                input_ids = d_input_ids.to(dg_model.device),
-                num_beams = gen_quantity*3,
+                input_ids=d_input_ids.to(dg_model.device),
+                num_beams=gen_quantity * 3,
                 length_penalty=0.9,
                 num_beam_groups=gen_quantity,
                 diversity_penalty=1.0,
-                num_return_sequences = gen_quantity*2
+                num_return_sequences=gen_quantity * 2,
             )
             for out_seq_ids in out_ids:
-                option = dg_tokenizer.decode(out_seq_ids,skip_special_tokens=True)
+                option = dg_tokenizer.decode(out_seq_ids, skip_special_tokens=True)
                 logger.info(f"{i} {option}")
                 all_options.append(option)
         # logger.info(all_options)
-        return self._selection(context,question,answer.tag,all_options, gen_quantity)
+        return self._selection(context, question, answer.tag, all_options, gen_quantity)
 
-    def _selection(self,context, question, answer, all_options, gen_quantity):
+    def _selection(self, context, question, answer, all_options, gen_quantity):
         max_combin = [0, []]
         for combin in set(it.combinations(all_options, gen_quantity)):
             options = list(combin) + [answer]
             keep = True
             for i in set(it.combinations(options, 2)):
-                a = "".join([char if char.isalpha() or char == " " else " " + char + " " for char in i[0]])
-                b = "".join([char if char.isalpha() or char == " " else " " + char + " " for char in i[1]])
+                a = "".join(
+                    [
+                        char if char.isalpha() or char == " " else " " + char + " "
+                        for char in i[0]
+                    ]
+                )
+                b = "".join(
+                    [
+                        char if char.isalpha() or char == " " else " " + char + " "
+                        for char in i[1]
+                    ]
+                )
                 metrics_dict = self.nlgeval.compute_individual_metrics([a], b)
-                if metrics_dict['Bleu_1'] > 0.20:
+                if metrics_dict["Bleu_1"] > 0.20:
                     keep = False
                     break
             if keep:
@@ -142,10 +192,25 @@ class BartDistractorGeneration():
                     encoding_input.append([prompt, choice])
                 encoding_input.append([prompt, answer])
                 labels = torch.tensor(len(options) - 1).unsqueeze(0)
-                encoding = self.tokenizer(encoding_input, return_tensors='pt', padding=True, truncation='only_first', max_length=max_length)
-                outputs = self.model(**{k: v.unsqueeze(0).to(self.model.device) for k, v in encoding.items()},
-                                labels=labels.to(self.model.device))  # batch size is 1
-                entropy = Categorical(probs=torch.softmax(outputs.logits, -1)).entropy().tolist()[0]
+                encoding = self.tokenizer(
+                    encoding_input,
+                    return_tensors="pt",
+                    padding=True,
+                    truncation="only_first",
+                    max_length=max_length,
+                )
+                outputs = self.model(
+                    **{
+                        k: v.unsqueeze(0).to(self.model.device)
+                        for k, v in encoding.items()
+                    },
+                    labels=labels.to(self.model.device),
+                )  # batch size is 1
+                entropy = (
+                    Categorical(probs=torch.softmax(outputs.logits, -1))
+                    .entropy()
+                    .tolist()[0]
+                )
                 if entropy >= max_combin[0]:
                     max_combin = [entropy, options]
         return max_combin[1][:-1]
